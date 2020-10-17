20ec6f572b3dca6ccacc18a2e975aa3919f49059
Marc-Colin <mcolin@gmail.com>
2020-10-17 10:15:46 -0400

    blah

--- BackEnd/handler.js
+++ BackEnd/handler.js
@@ -1,16 +1,6 @@
 function interpretEmail(email) {
     //this will change the email into a readable format to process
-<<<<<<< HEAD
-    var txt = str;
-    var score = 0;
-    var phrase = "testing phrase";
-    var pos = txt.indexOf(phrase);
 
-    if(pos >= 0) {
-        score++;
-    }
-=======
-    var strEmail = "";
     return strEmail;
 }
 
@@ -26,5 +16,4 @@ function calcuateScore() {
 function findWord(email, words) {
     //Find the words with the body 
 
->>>>>>> ac2826edf0b58401f2aaca00f72e5f89e45dbbae
 }
