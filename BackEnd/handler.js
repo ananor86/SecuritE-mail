76ead949ccf3dc4f9bef4645c919e31e10efa275
Marc-Colin <mcolin@gmail.com>
2020-10-17 09:56:01 -0400

    changed stuff in handler

--- BackEnd/handler.js
+++ BackEnd/handler.js
@@ -1,4 +1,11 @@
 function interpretEmail(str) {
     //this will change the email into a readable format to process
-    
+    var txt = str;
+    var score = 0;
+    var phrase = "testing phrase";
+    var pos = txt.indexOf(phrase);
+
+    if(pos >= 0) {
+        score++;
+    }
 }
