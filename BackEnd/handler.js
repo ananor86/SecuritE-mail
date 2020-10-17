ac2826edf0b58401f2aaca00f72e5f89e45dbbae
ananor86 <ananor86@students.rowan.edu>
2020-10-17 09:54:32 -0400

    backend frame part 2

--- BackEnd/handler.js
+++ BackEnd/handler.js
@@ -1,4 +1,19 @@
-function interpretEmail(str) {
+function interpretEmail(email) {
     //this will change the email into a readable format to process
-    
+    var strEmail = "";
+    return strEmail;
+}
+
+function toHash() {
+    //hash given string
+}
+
+function calcuateScore() {
+    //calculate the score of all given words
+}
+
+
+function findWord(email, words) {
+    //Find the words with the body 
+
 }
