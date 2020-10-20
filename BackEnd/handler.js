1503e9305804879700b5e357b47562405c7e7a15
ananor86 <ananor86@students.rowan.edu>
2020-10-20 12:48:46 -0400

    Fixed Function call to wrong one

--- BackEnd/handler.js
+++ BackEnd/handler.js
@@ -26,4 +26,4 @@ function findWord(email, words) {
 
 }
 
-console.log(interpretEmail("3fdsafadsfsd testing phrase hello world banana bread These are some test words woohoo"));
+console.log(calcuateScore("3fdsafadsfsd testing phrase hello world banana bread These are some test words woohoo"));
