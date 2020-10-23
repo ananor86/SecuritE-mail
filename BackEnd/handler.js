90d24e269181d4597a2ede907ad00af2a08f772b
ananor86 <ananor86@students.rowan.edu>
2020-10-23 13:05:11 -0400

    added to handler.js and documented the code

--- BackEnd/handler.js
+++ BackEnd/handler.js
@@ -1,6 +1,11 @@
 //import { connection } from "./connection";
 //import { mysql, connect } from './connection.js'
 
+/**
+ * Converts the email into a readable form for
+ * the application to be able to use
+ * @param {Email object} email 
+ */
 function interpretEmail(email) {
     //this will change the email into a readable format to process
 
@@ -9,20 +14,37 @@ function interpretEmail(email) {
     return score;
 }
 
-function toHash(item) {
+/**
+ * Takes the email and changes it from plaintext
+ * to a hashed version found inside the database
+ * @param {Email Object} email 
+ */
+function toHash(email) {
     //hash given string
+    var plaintxt = interpretEmail(email);
 
 }
 
+/**
+ * Given an email this will scan through and return the appropriate score
+ * 
+ * @param {Email Object} email 
+ * @param {Array} phrases 
+ */
 function calcuateScore(email, phrases) {
     //calculate the score of all given words
-    var txt = email;
+    var txt = toHash(interpretEmail(email));
     var score = 0;
-    var phrase = "testing phrase";
-    var pos = txt.indexOf(phrase);
-    if(pos >= 0) {
-        score++;
+
+    //for each phrase inside the array
+    //find an occurence and update the score
+    for(i = 0; i < phrases.length; i++) {
+        var pos = txt.indexOf(phrases[i]);
+        if(pos >= 0) {
+            score++;
+        }
     }
+
     return score;
 }
 
@@ -32,5 +54,5 @@ function findWord(email, words) {
 
 }
 
-console.log(interpretEmail("3fdsafadsfsd testing phrase hello world banana bread fortnite minecraft i am dead inside woohoo"));
+//console.log(interpretEmail("3fdsafadsfsd testing phrase hello world banana bread fortnite minecraft i am dead inside woohoo"));
 
