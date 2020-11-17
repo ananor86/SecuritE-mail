f99ef599080949ec0df90de3756a27d5e9232458
gouldt86 <gouldt86@students.rowan.edu>
2020-11-17 13:46:03 -0500

    updated tunnel and removed obsolete code

--- public/tunnel.js
+++ public/tunnel.js
@@ -1,29 +1,10 @@
 const fs = require('fs');
-const mysql = require('mysql');
 const Client = require('ssh2');
 const conn = new Client()
-const { SSL_OP_TLS_BLOCK_PADDING_BUG } = require('constants');
-
 
 
 var key = fs.readFileSync(__dirname + '/keys/EmailScan.pem', 'utf-8')
-var phrases = [];
 
-var connection = mysql.createConnection({
-    host: "ec2-52-91-199-44.compute-1.amazonaws.com",
-    user: "SYSTEM_USER",
-    password: "SecuritE-mail",
-    port: "3306",
-    database: "phrases"
-
-});
-
-var config = {
-    username:'ubuntu',
-    host:'52.91.199.44',
-    dstPort: 8080,
-    privateKey: key
- };
 
 var score = 0;
 function server(command) {
@@ -51,5 +32,4 @@ function server(command) {
 
     setTimeout(() => { module.exports.score = score; }, 2000);
 }
-
 module.exports.server = server;
